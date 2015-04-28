bf4d241a22a768310f48c0ad5a70601c5199b8c8
TZ <atian25@qq.com>
2015-04-28 13:51:21 +0800

    err handler

--- server/controller/blog/blog.js
+++ server/controller/blog/blog.js
@@ -15,19 +15,25 @@ router.get('/', function(req, res, next){
     },
     json: true
   }, function(err, response, body){
-    var data = {
-      name: body[0].column.name,
-      list: body.map(function (item) {
-        return {
-          id: item.slug,
-          title: item.title,
-          imgUrl: item.titleImage,
-          publishedTime: item.publishedTime,
-          summary: item.summary
-        }
-      })
-    };
-    res.render('blog/list', data)
+    if(err){
+      next(err);
+    }else if(response.statusCode !== 200){
+      res.status(response.statusCode).end();
+    }else {
+      var data = {
+        name: body[0].column.name,
+        list: body.map(function (item) {
+          return {
+            id: item.slug,
+            title: item.title,
+            imgUrl: item.titleImage,
+            publishedTime: item.publishedTime,
+            summary: item.summary
+          }
+        })
+      };
+      res.render('blog/list', data);
+    }
   });
 });
 
@@ -36,25 +42,31 @@ router.get('/:id', function(req, res, next){
     url: 'http://zhuanlan.zhihu.com/api/columns/zuimei/posts/' + req.params.id,
     json: true
   }, function(err, response, body){
-    var meta = body.meta;
-    var data = {
-      id: body.slug,
-      name: body.title,
-      imgUrl: body.titleImage,
-      publishedTime: body.publishedTime,
-      summary: body.summary,
-      content: body.content,
-      links: {
-        next: meta.next && {
-          name: meta.next.title,
-          id: meta.next.url.match(/(\d+)$/)[1]
-        },
-        previous: meta.previous && {
-          name: meta.previous.title,
-          id: meta.previous.url.match(/(\d+)$/)[1]
+    if(err){
+      next(err);
+    }else if(response.statusCode !== 200){
+      res.status(response.statusCode).end();
+    }else {
+      var meta = body.meta;
+      var data = {
+        id: body.slug,
+        name: body.title,
+        imgUrl: body.titleImage,
+        publishedTime: body.publishedTime,
+        summary: body.summary,
+        content: body.content,
+        links: {
+          next: meta.next && {
+            name: meta.next.title,
+            id: meta.next.url.match(/(\d+)$/)[1]
+          },
+          previous: meta.previous && {
+            name: meta.previous.title,
+            id: meta.previous.url.match(/(\d+)$/)[1]
+          }
         }
-      }
-    };
-    res.render('blog/list', data)
+      };
+      res.render('blog/list', data);
+    }
   });
 });
