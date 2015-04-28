2e769b7511e590789404491cced9087f7c9b7772
TZ <atian25@qq.com>
2015-04-28 11:53:20 +0800

    query data

--- server/controller/blog/blog.js
+++ server/controller/blog/blog.js
@@ -7,18 +7,54 @@ var request = require('request');
 module.exports = router;
 
 router.get('/', function(req, res, next){
-  var data = {
-    name: '前端外刊评论',
-    list: [
-      {id: '', title: '', imgUrl: '', author:'' },
-      {id: '', title: '', imgUrl: '', author:'' }
-    ]
-  };
-  res.render('blog/list', data);
+  request.get({
+    url: 'http://zhuanlan.zhihu.com/api/columns/zuimei/posts',
+    qs: {
+      limit: 10,
+      offset: (req.params.page || 0) * 10
+    },
+    json: true
+  }, function(err, response, body){
+    var data = {
+      name: body[0].column.name,
+      list: body.map(function (item) {
+        return {
+          id: item.slug,
+          title: item.title,
+          imgUrl: item.titleImage,
+          publishedTime: item.publishedTime,
+          summary: item.summary
+        }
+      })
+    };
+    res.render('blog/list', data)
+  });
 });
 
 router.get('/:id', function(req, res, next){
-  var id = req.params.id;
-  var data = {id: '', title: '', imgUrl: '', author:'' };
-  res.render('blog/detail', data);
+  request({
+    url: 'http://zhuanlan.zhihu.com/api/columns/zuimei/posts/' + req.params.id,
+    json: true
+  }, function(err, response, body){
+    var meta = body.meta;
+    var data = {
+      id: body.slug,
+      name: body.title,
+      imgUrl: body.titleImage,
+      publishedTime: body.publishedTime,
+      summary: body.summary,
+      content: body.content,
+      links: {
+        next: meta.next && {
+          name: meta.next.title,
+          id: meta.next.url.match(/(\d+)$/)[1]
+        },
+        previous: meta.previous && {
+          name: meta.previous.title,
+          id: meta.previous.url.match(/(\d+)$/)[1]
+        }
+      }
+    };
+    res.render('blog/list', data)
+  });
 });
