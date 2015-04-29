360695d811e2bc294d0012e7b839168c6f1be67d
TZ <atian25@qq.com>
2015-04-29 10:54:41 +0800

    数据存储本地

--- server/controller/blog/blog.js
+++ server/controller/blog/blog.js
@@ -4,38 +4,41 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var testData = require('./test-data.json');
+var listData = JSON.parse(JSON.stringify(testData)).map(function(item){
+  item.name = item.title;
+  delete item.content;
+  delete item.links;
+  return item;
+});
+
 module.exports = router;
 
 router.get('/', function(req, res, next){
-  request.get({
-    url: 'http://zhuanlan.zhihu.com/api/columns/zuimei/posts',
-    qs: {
-      limit: 10,
-      offset: (req.query.page || 0) * 10
-    },
-    json: true
-  }, function(err, response, body){
-    if(err){
-      next(err);
-    }else if(response.statusCode !== 200){
-      res.status(response.statusCode).end(body);
-    }else {
-      var data = {
-        name: body[0].column.name,
-        list: body.map(function (item) {
-          return {
-            id: item.slug,
-            title: item.title,
-            imgUrl: replaceImage(item.titleImage),
-            publishedTime: item.publishedTime,
-            summary: replaceImage(item.summary)
-          }
-        })
-      };
-      res.locals.title = data.name;
-      res.render('blog/blog', data);
+  var offset = (req.query.page || 0) * 10;
+  var data = {
+    name: '最美应用 | 有价值的好应用',
+    list: listData.slice(offset, offset + 10)
+  };
+  res.locals.title = data.name;
+  res.render('blog/blog', data);
+});
+
+router.get('/:id', function(req, res, next){
+  var id = req.params.id;
+  var data;
+  for(var i = 0,len=testData.length; i<len; i++){
+    if(testData[i].id == id){
+      data = testData[i];
+      break;
     }
-  });
+  }
+  if(data) {
+    res.locals.title = data.name;
+    res.render('blog/blog', data);
+  }else{
+    res.status(404).send('No Found blog id=' + id);
+  }
 });
 
 router.get('/img/:id', function(req, res, next){
@@ -43,42 +46,3 @@ router.get('/img/:id', function(req, res, next){
     url: 'http://pic1.zhimg.com/' + req.params.id
   }).pipe(res);
 });
-
-router.get('/:id', function(req, res, next){
-  request({
-    url: 'http://zhuanlan.zhihu.com/api/columns/zuimei/posts/' + req.params.id,
-    json: true
-  }, function(err, response, body){
-    if(err){
-      next(err);
-    }else if(response.statusCode !== 200){
-      res.status(response.statusCode).end(body);
-    }else {
-      var meta = body.meta;
-      var data = {
-        id: body.slug,
-        name: body.title,
-        imgUrl: replaceImage(body.titleImage),
-        publishedTime: body.publishedTime,
-        content: replaceImage(body.content),
-        links: {
-          next: meta.next && {
-            name: meta.next.title,
-            id: meta.next.url.match(/(\d+)$/)[1]
-          },
-          previous: meta.previous && {
-            name: meta.previous.title,
-            id: meta.previous.url.match(/(\d+)$/)[1]
-          }
-        }
-      };
-      res.locals.title = data.name;
-      res.render('blog/blog', data);
-    }
-  });
-});
-
-
-function replaceImage(url){
-  return url && url.replace(/http:\/\/pic\d+\.zhimg.com\//g, '/blog/img/');
-}
